81f67035cafa5f6f6c2f8073bbc3d9ac0ed2dadb
TchokoApps <tchokoapps@gmail.com>
2022-01-05 10:25:46 +0100

    001-initial project

---
+++ Springboot-and-Thymeleaf/src/main/java/com/tchokoapps/spring/boot/SpringbootandThymeleaf/SpringbootAndThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.tchokoapps.spring.boot.SpringbootandThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAndThymeleafApplication.class, args);
+	}
+
+}
---
+++ Springboot-and-Thymeleaf/src/test/java/com/tchokoapps/spring/boot/SpringbootandThymeleaf/SpringbootAndThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchokoapps.spring.boot.SpringbootandThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAndThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
