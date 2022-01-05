a0107f20071aa82b3697317f76d3ad11721903de
TchokoApps <tchokoapps@gmail.com>
2022-01-05 19:18:12 +0100

    004-Adding Tailwind

--- Springboot-and-Thymeleaf/src/main/java/com/tchokoapps/spring/boot/SpringbootandThymeleaf/controller/RootController.java
+++ Springboot-and-Thymeleaf/src/main/java/com/tchokoapps/spring/boot/SpringbootandThymeleaf/controller/RootController.java
@@ -1,2 +1,17 @@
-package com.tchokoapps.spring.boot.SpringbootandThymeleaf.controller;public class RootController {
+package com.tchokoapps.spring.boot.SpringbootandThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class RootController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("pageTtitle", "Springboot Thymeleaf");
+        model.addAttribute("scientists", List.of("Albert Einstein", "Niels Bohr", "James Clerk Maxwell"));
+        return "index";
+    }
 }
